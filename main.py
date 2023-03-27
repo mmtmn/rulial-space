09ebffd6777a154999a3ca90f23d5a91c1cc0439
mmtmn <thiagomnobrega@gmail.com>
2023-03-27 17:44:46 -0300

    Add animations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from itertools import product
 import networkx as nx
+import matplotlib.animation as animation
 
 
 class TuringMachine:
@@ -37,7 +38,7 @@ tm_list = generate_turing_machines(1, 2)
 
 
 
-def create_rulial_space_graph(turing_machines, steps):
+def create_rulial_space_graph(turing_machines, steps, step_limit):
     rulial_space_graph = nx.DiGraph()
     rulial_space_graph.add_nodes_from(turing_machines)
     
@@ -46,23 +47,35 @@ def create_rulial_space_graph(turing_machines, steps):
             tm1_copy = TuringMachine(tm1.rules, tm1.state, tm1.position, tm1.tape.copy())
             tm2_copy = TuringMachine(tm2.rules, tm2.state, tm2.position, tm2.tape.copy())
             
-            try:
-                tm1_copy.step()
-                tm2_copy.step()
+            for _ in range(step_limit):
+                try:
+                    tm1_copy.step()
+                    tm2_copy.step()
+                except Exception:
+                    break
                 
-                if tm1_copy.state == tm2_copy.state and tm1_copy.tape == tm2_copy.tape:
-                    rulial_space_graph.add_edge(tm1, tm2)
-            except Exception:
-                pass
+            if tm1_copy.state == tm2_copy.state and tm1_copy.tape == tm2_copy.tape:
+                rulial_space_graph.add_edge(tm1, tm2)
                 
     return rulial_space_graph
-  
-rulial_space = create_rulial_space_graph(tm_list, 1)
 
 
-def visualize_rulial_space(graph):
-    pos = nx.spring_layout(graph)
-    nx.draw(graph, pos, node_size=50, with_labels=False, node_color="blue", font_size=8)
+def visualize_rulial_space_animation(turing_machines, steps):
+    fig, ax = plt.subplots()
+    ani = animation.FuncAnimation(fig, update_rulial_space, frames=steps, fargs=(turing_machines, ax), interval=1000, blit=False)
     plt.show()
 
-visualize_rulial_space(rulial_space)
+def update_rulial_space(step, turing_machines, ax):
+    ax.clear()
+    rulial_space = create_rulial_space_graph(turing_machines, step, step)
+    pos = nx.spring_layout(rulial_space)
+    nx.draw(rulial_space, pos, node_size=50, with_labels=False, node_color="blue", font_size=8, ax=ax)
+    ax.set_title("Step: {}".format(step))
+
+while True:
+    steps = int(input("Enter the number of steps: "))
+    visualize_rulial_space_animation(tm_list, steps)
+
+    exit = input("Do you want to exit? (y/n): ")
+    if exit == "y":
+        break
